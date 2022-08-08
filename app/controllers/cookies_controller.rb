71da2b772641bc79b35aa67a297ed4f570d5a4d3
tf-jlemasters <jlemasters@tracefirst.com>
2022-08-08 16:12:48 -0600

    fixed cookies typo

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,6 +1,6 @@
 class CookiesController < ApplicationController
   
   def index
-    session[:cookies_accepts] = params[:cookies_accepted] if params[:cookies_accepted]
+    session[:cookies_accepted] = params[:cookies_accepted] if params[:cookies_accepted]
     end
   end

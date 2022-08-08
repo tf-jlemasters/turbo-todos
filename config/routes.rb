b6cbe7b205d882c2db2f0c77b63caa59d430eb1f
tf-jlemasters <jlemasters@tracefirst.com>
2022-08-08 13:23:35 -0600

    added controller/route for cookies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cookies', to: 'cookies#index'
   devise_for :users
   resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
